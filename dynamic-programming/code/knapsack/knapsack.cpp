befa91254a9fc1f5d539752aa0abfee0eabafab8
Sean Daniels <sdaniels8864@gmail.com>
2021-03-11 08:23:13 -0500

    upd

--- dynamic-programming/code/knapsack/knapsack.cpp
+++ dynamic-programming/code/knapsack/knapsack.cpp
@@ -9,26 +9,6 @@ int getMax(int colIndex, int rowIndex,
 int main(){
     vector<int> values = {60, 100, 120};
     vector<int> weights = {10, 20, 30};
-    vector<vector<int>> table;
-    unordered_map<int, int> valueMap;
-    int granularity = 10, maxWeight = 50;
-    int numberOfColumns = maxWeight/granularity;
-    int numberOfRows = values.size()-1;
-    vector<int> tempValues, tempMax;
-    for(int i = 0; i < values.size(); i++){
-        auto mapIterator = valueMap.find(values[i]);
-        if(mapIterator==valueMap.end()){
-            valueMap.insert({values[i], weights[i]});
-        }
-    }
-
-    tempValues.push_back(values[0]);
-    int tempWeight = valueMap.find(values[0])->second;
-    for(int i = 0; i < numberOfColumns; i++){
-        int currWeight = granularity*i;
-        if(currWeight < tempWeight) tempMax.push_back(0);
-        else if(currWeight)
-    }
 
 
 }
