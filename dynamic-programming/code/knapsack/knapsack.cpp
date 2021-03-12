78150bbc98c2ab4b35c58dc2141802dbaeded4f3
Sean Daniels <sdaniel5@ncsu.edu>
2021-03-11 20:30:25 -0500

    upd

--- dynamic-programming/code/knapsack/knapsack.cpp
+++ dynamic-programming/code/knapsack/knapsack.cpp
@@ -42,19 +42,17 @@ int main(){
     vector<int> values = {60, 100, 120};
     vector<int> weights = {10, 20, 30};
     vector<int> capacities;
+    // initialize a capacities vector
+    // this needs to be fixed, the goal is to be able to pass a weight as an argument
     for(int i = 0; i < 6; i++){
         capacities.push_back(10*i);
     }
     cout << "Capacities: " << endl;
-    for(auto x : capacities){
-        cout << x;
-        if(x==capacities.back()) cout << endl;
-        else cout << ", ";
-    }
     vector<vector<int>> dp(values.size(), vector<int>(capacities.size()));
     cout << "Un-initialized 0 table:" << endl;
     printVectorOfVectors(dp);
-
+    // initialize first row of table
+    // this corresponds with only being able to pick the first item in the list
     for(size_t i = 0; i < capacities.size(); i++){
         if(capacities[i]>=weights[0]){
             dp[0][i] = values[0];
@@ -64,20 +62,20 @@ int main(){
     cout << "Initialized table(first row set):" << endl;
     printVectorOfVectors(dp);
 
+    // i,j = row,col
+    // start at 1 for both -> row 0 has already been set (single item case), col 0 has already been set (0 capacity case)
     for(size_t i = 1; i < values.size(); i++){
         for(size_t j = 1; j < capacities.size(); j++){
             int valueA = 0, valueB = 0;
             if(weights[i] <= capacities[j]){
-                cout << "Current capacity: " << capacities[j] << endl;
-                cout << "Weight of this item: " << weights[i] << endl;
+                // this is only valid for this weight/capacity relationship.
                 int colIndex = (capacities[j] - weights[i])/10;
-                cout << "Using col index: " << colIndex << endl;
+                // include this item
+                // the value will be this items value + the maximum determined value at this capacity prior to this item
                 valueA = values[i] + dp[i-1][colIndex];
-                cout << "Possible value(a): " << valueA << endl;
             }
-            cout << "Generating possible value from index location i,j: " << i-1 << "," << j << endl;
+            // exclude this item
             valueB = dp[i-1][j];
-            cout << "Possible value(b): " << valueB << endl;
             dp[i][j] = max(valueA, valueB);
         }
     }
