da7b8a6c85fe54c1f58e6ae3a761f4a526487f94
Sean Daniels <sdaniels8864@gmail.com>
2021-04-21 14:16:43 -0400

    Format/update
    auto formatted
    Added code to locate shorter cycles contained within longer cycles

--- graphs/traversal/graphTraversal.cpp
+++ graphs/traversal/graphTraversal.cpp
@@ -2,169 +2,196 @@
 
 using namespace std;
 
-class Graph{
-    public:
-        int numberOfNodes;
-        vector<vector<int>> adjacencyList, cycleList;
-        vector<int> visitedTable, unvisited, visitedSet;
-        vector<bool> boolVisited, boolCycle;
+class Graph {
+public:
+  int numberOfNodes;
+  vector<vector<int>> adjacencyList, cycleList;
+  vector<int> visitedTable, unvisited, visitedSet, backEdges;
+  vector<bool> boolVisited, boolCycle;
 
-        Graph(int nodes){
-            numberOfNodes = nodes;
-            adjacencyList = vector<vector<int>> (nodes, vector<int>(nodes));
-            for(int i = 0; i < numberOfNodes; i++){
-                visitedTable.push_back(0);
-                boolVisited.push_back(false);
-                boolCycle.push_back(false);
-            }
-        }
+  Graph(int nodes) {
+    numberOfNodes = nodes;
+    adjacencyList = vector<vector<int>>(nodes, vector<int>(nodes));
+    for (int i = 0; i < numberOfNodes; i++) {
+      visitedTable.push_back(0);
+      boolVisited.push_back(false);
+      boolCycle.push_back(false);
+    }
+  }
 
-        void addEdge(int src, int dst){
-          adjacencyList[src][dst] = 1;
-        }
+  void addEdge(int src, int dst) { adjacencyList[src][dst] = 1; }
 
-        void printAdjacencyList(){
-            for(auto x : adjacencyList){
-                for(auto y = x.begin(); y != x.end(); y++){
-                cout << *y;
-                if(y==x.end()-1)cout << endl;
-                else cout << ", ";
-                }
-            }
-        }
-
-        vector<int> *getAdjacency(int thisNode){
-            return &adjacencyList[thisNode];
-        }
-
-        bool isVisited(int node){
-            auto found = find(visitedSet.begin(), visitedSet.end(), node);
-            if(found != visitedSet.end()) return true;
-            return false;
-        }
-
-        void dfs_b(int targetNode) {
-          boolVisited[targetNode] = true;
-          boolCycle[targetNode] = true;
-          vector<int> currentNodeAdjacenyList = adjacencyList[targetNode];
-          for(int i = 0; i < numberOfNodes; i++){
-            // if there is a an edge from the current node to some other node, and the other node has not been visited, visit it
-              if(currentNodeAdjacenyList[i] && !boolVisited[i]){
-                  dfs_b(i);
-              }
-              // if there is an edge from the current node to some other node, and the other node *has* been visited
-              else if(currentNodeAdjacenyList[i] && boolVisited[i]){
-                // if the current node has been visited during the current path traversal, and the current node is not the
-                if( i != targetNode && boolCycle[i]){
-                  vector<int> thisCycle;
-                  // a cycle has been found
-                  for(int j = 0; j < numberOfNodes; j++){
-                    // for all nodes in this cycle, add to a temp cycle list
-                    if(boolCycle[j]) thisCycle.push_back(j);
-                  }
-                  // add the iterated to node to the cycle
-                  thisCycle.push_back(i);
-                  // add this cycle to collection of cycles
-                  cycleList.push_back(thisCycle);
-                }
-              }
-          }
-          // remove this node from cycle candidacy
-          boolCycle[targetNode] = false;
-        }
-
-        void dfs_a(int targetNode){
-
-            if(isVisited(targetNode)){
-                cout << "Node " << targetNode << " has already been visited" << endl;
-                return;
-            }
-
-            else{
-                cout << "Node " << targetNode << " has not been visited" << endl;
-                // add node to adjacency list
-                vector<int> tempAdjacencyList = adjacencyList[targetNode];
-                cout << "Adding edges of current node to 'unvisited' list" << endl;
-                // add edges of current node to unvisited list (assume nodes aren't visited)
-                for(int i = 0; i < numberOfNodes; i++){
-                    if(tempAdjacencyList[i]!=0) unvisited.push_back(i);
-                }
-                cout << "Adding node " << targetNode << " to 'visited' list" << endl;
-                // add current node to list of visited nodes
-                visitedSet.push_back(targetNode);
-                // iterate through this node's edges
-                while(unvisited.size()>0){
-                    cout << "Visiting back node of 'unvisited' list" << endl;
-                    // get next node
-                    int next = unvisited.back();
-                    cout << "Removing back node of 'unvisited' list" << endl;
-                    // pop next node from unvisited list
-                    unvisited.pop_back();
-                    // visit this next node
-                    dfs_a(next);
-                }
-
-            }
-        }
-
-        bool cycleUtility(int targetNode, bool visited[], bool *recStack) {
-          if (visited[targetNode] == false) {
-            visited[targetNode] = true;
-            recStack[targetNode] = true;
-            for (auto it = adjacencyList[targetNode].begin();
-                 it != adjacencyList[targetNode].end(); it++) {
-              if (!visited[*it] && cycleUtility(*it, visited, recStack)) {
-                return true;
-              } else if (recStack[*it]) {
-                return true;
-              }
-            }
-          }
-          recStack[targetNode] = false;
-          return false;
-        }
-
-        bool hasCycle() {
-          bool *visited = new bool[numberOfNodes];
-          bool *recStack = new bool[numberOfNodes];
-          for (int i = 0; i < numberOfNodes; i++) {
-            visited[i] = false;
-            recStack[i] = false;
-          }
-          for (int i = 0; i < numberOfNodes; i++) {
-            cout << numberOfNodes << endl;
-            cout << "Checking for cycle with node " << i << endl;
-            if (cycleUtility(i, visited, recStack))
-              return true;
-          }
-          return false;
-        }
-};
-
-int main(){
-
-    // create graph with 5 nodes (node 1 through node 5)
-    // add edge from node 1 to node 5
-    /* [Node 0] - [Node 1] - [Node 2]
-                  /      \           //
-           [Node 3] - [Node 4]       */
-
-    Graph myGraph(5);
-    myGraph.addEdge(0,1);
-    myGraph.addEdge(1,2);
-    myGraph.addEdge(1,3);
-    myGraph.addEdge(1,4);
-    myGraph.addEdge(3,4);
-    myGraph.addEdge(3,0);
-    myGraph.addEdge(4,0);
-    myGraph.printAdjacencyList();
-    myGraph.dfs_b(0);
-    for(auto x: myGraph.cycleList){
-      for(auto it = x.begin(); it != x.end(); ++it){
-        cout << *it;
-        if(it+1 == x.end()) cout << endl;
-        else cout << "->";
+  void printAdjacencyList() {
+    for (auto x : adjacencyList) {
+      for (auto y = x.begin(); y != x.end(); y++) {
+        cout << *y;
+        if (y == x.end() - 1)
+          cout << endl;
+        else
+          cout << ", ";
       }
     }
-    return 0;
+  }
+
+  bool isVisited(int node) {
+    auto found = find(visitedSet.begin(), visitedSet.end(), node);
+    if (found != visitedSet.end())
+      return true;
+    return false;
+  }
+
+  bool isBackEdge(int node) {
+    auto it = find(backEdges.begin(), backEdges.end(), node);
+    if (it != backEdges.end())
+      return true;
+    return false;
+  }
+  void dfs_b(int targetNode) {
+    boolVisited[targetNode] = true;
+    boolCycle[targetNode] = true;
+    vector<int> currentNodeAdjacenyList = adjacencyList[targetNode];
+    for (int i = 0; i < numberOfNodes; i++) {
+      // if there is a an edge from the current node to some other node, and the
+      // other node has not been visited, visit it
+      if (currentNodeAdjacenyList[i] && !boolVisited[i]) {
+        dfs_b(i);
+      }
+      // if there is an edge from the current node to some other node, and the
+      // other node *has* been visited
+      else if (currentNodeAdjacenyList[i] && boolVisited[i]) {
+        // if the current node has been visited during the current path
+        // traversal, and the current node is not the
+        vector<int> thisCycle;
+        if (i != targetNode && boolCycle[i]) {
+          cout << "Back edge from node " << targetNode << " to node " << i
+               << endl;
+          backEdges.push_back(targetNode);
+          // a cycle has been found
+          for (int j = 0; j < numberOfNodes; j++) {
+            // for all nodes in this cycle, add to a temp cycle list
+            if (boolCycle[j])
+              thisCycle.push_back(j);
+          }
+          // add front node to back, for clarity while printing
+          thisCycle.push_back(thisCycle[0]);
+          // add this cycle to collection of cycles
+          cycleList.push_back(thisCycle);
+          thisCycle.clear();
+        }
+        if (i != targetNode && isBackEdge(i)) {
+          cout << "Current node " << targetNode
+               << " has path to known back edge (" << i << ")" << endl;
+          for (int j = 0; j < numberOfNodes; j++) {
+            if (boolCycle[j])
+              thisCycle.push_back(j);
+          }
+          // add back edge src node to cycle
+          thisCycle.push_back(i);
+          // add front node to back, for clarity while printing
+          thisCycle.push_back(thisCycle[0]);
+          cycleList.push_back(thisCycle);
+          thisCycle.clear();
+        }
+      }
+    }
+    // remove this node from cycle candidacy
+    boolCycle[targetNode] = false;
+  }
+
+  void dfs_a(int targetNode) {
+
+    if (isVisited(targetNode)) {
+      cout << "Node " << targetNode << " has already been visited" << endl;
+      return;
+    }
+
+    else {
+      cout << "Node " << targetNode << " has not been visited" << endl;
+      // add node to adjacency list
+      vector<int> tempAdjacencyList = adjacencyList[targetNode];
+      cout << "Adding edges of current node to 'unvisited' list" << endl;
+      // add edges of current node to unvisited list (assume nodes aren't
+      // visited)
+      for (int i = 0; i < numberOfNodes; i++) {
+        if (tempAdjacencyList[i] != 0)
+          unvisited.push_back(i);
+      }
+      cout << "Adding node " << targetNode << " to 'visited' list" << endl;
+      // add current node to list of visited nodes
+      visitedSet.push_back(targetNode);
+      // iterate through this node's edges
+      while (unvisited.size() > 0) {
+        cout << "Visiting back node of 'unvisited' list" << endl;
+        // get next node
+        int next = unvisited.back();
+        cout << "Removing back node of 'unvisited' list" << endl;
+        // pop next node from unvisited list
+        unvisited.pop_back();
+        // visit this next node
+        dfs_a(next);
+      }
+    }
+  }
+
+  bool cycleUtility(int targetNode, bool visited[], bool *recStack) {
+    if (visited[targetNode] == false) {
+      visited[targetNode] = true;
+      recStack[targetNode] = true;
+      for (auto it = adjacencyList[targetNode].begin();
+           it != adjacencyList[targetNode].end(); it++) {
+        if (!visited[*it] && cycleUtility(*it, visited, recStack)) {
+          return true;
+        } else if (recStack[*it]) {
+          return true;
+        }
+      }
+    }
+    recStack[targetNode] = false;
+    return false;
+  }
+
+  bool hasCycle() {
+    bool *visited = new bool[numberOfNodes];
+    bool *recStack = new bool[numberOfNodes];
+    for (int i = 0; i < numberOfNodes; i++) {
+      visited[i] = false;
+      recStack[i] = false;
+    }
+    for (int i = 0; i < numberOfNodes; i++) {
+      cout << numberOfNodes << endl;
+      cout << "Checking for cycle with node " << i << endl;
+      if (cycleUtility(i, visited, recStack))
+        return true;
+    }
+    return false;
+  }
+};
+
+int main() {
+
+  // create graph with 5 nodes (node 1 through node 5)
+  // add edge from node 1 to node 5
+  /* [Node 0] - [Node 1] - [Node 2]
+                /      \           //
+         [Node 3] - [Node 4]       */
+
+  Graph myGraph(5);
+  myGraph.addEdge(0, 1);
+  myGraph.addEdge(1, 2);
+  myGraph.addEdge(1, 3);
+  myGraph.addEdge(1, 4);
+  myGraph.addEdge(3, 4);
+  myGraph.addEdge(4, 0);
+  myGraph.printAdjacencyList();
+  myGraph.dfs_b(0);
+  for (auto x : myGraph.cycleList) {
+    for (auto it = x.begin(); it != x.end(); ++it) {
+      cout << *it;
+      if (it + 1 == x.end())
+        cout << endl;
+      else
+        cout << "->";
+    }
+  }
+  return 0;
 }
